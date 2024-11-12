423eea1c429d9551a9883a00e6b47fd148deb6d0
Darsh Patel <93569122+darshpatel2714@users.noreply.github.com>
2024-11-12 23:24:16 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            // Removed deprecated options
         });
         console.log('MongoDB connected');
     } catch (error) {
@@ -11,4 +11,5 @@ const connectDB = async () => {
         process.exit(1);
     }
 };
+
 module.exports = connectDB;
